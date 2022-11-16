f30cad2dcb7c5fb2149a9dea13060db07c61b007
Michael Kaminski <michael_kaminski@outlook.com>
2022-11-16 22:45:27 +0900

    react init file cleanup + .env and gitignore added

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+port = process.env.PORT
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+  
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
