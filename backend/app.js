ab5fa02d3590b041e1a08bd0b5a904e2c847655f
Michael Kaminski <michael_kaminski@outlook.com>
2022-11-18 11:28:27 +0900

    feat: 'in-app connection to mongodb added'

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,23 @@
 import * as dotenv from 'dotenv'
 import express from 'express'
+import mongoose from 'mongoose'
+
+
 dotenv.config()
 const app = express()
 
+const connectToMongoCluster = async () => {
+try{
+    await mongoose.connect(process.env.MONGO_CONNECTION_URL)
+    console.log('Successfully connected to MongoDB cluster')
+} catch (error) {
+    throw error
+}
+}
+
+mongoose.connection.on('Disconnected', () => {
+    console.log('Disconnected from MongoDB cluster')
+})
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -10,6 +25,7 @@ app.get('/', (req, res) => {
   
   app.listen(process.env.PORT, (error) =>{
     if(!error){
+        connectToMongoCluster()
         console.log(`Server is Successfully Running on ${process.env.PORT}`)}
     else
         console.log("Error occurred, server can't start", error);
