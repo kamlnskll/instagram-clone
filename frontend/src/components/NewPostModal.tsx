f5b87ea7cac3c87e65af4ae048e318295c0aa3fd
Michael Kaminski <michael_kaminski@outlook.com>
2022-12-05 19:39:18 +0900

    feat: added conditional rendering (multistep form)

--- frontend/src/components/NewPostModal.tsx
+++ frontend/src/components/NewPostModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Uploader from './Uploader'
 
 
@@ -7,19 +7,20 @@ isOpen: boolean,
 }
 
 const NewPostModal = ({isOpen}: Props) => {
+
+const [addCaption, setAddCaption] = useState(false)
+
   return (
     isOpen ? (
     <div className='absolute bg-white rounded-2xl h-1/2 w-1/2 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
-    <h1 className='font-bold text-center py-2 border-b'>Create a new post</h1>
+    {addCaption ? <h1 className='font-bold text-center py-2 border-b'>Add a caption</h1> : <h1 className='font-bold text-center py-2 border-b'>Create a new post</h1> }
     <div className='h-full flex'>
     <div className='my-auto mx-auto'>
-    <h1 className='text-xl tracking-wide'>Drag photo and videos here</h1>
-    <Uploader />
+    {addCaption ? <h1>Caption Space Here + IMG PREVIEW</h1> : <> <h1 className='text-xl tracking-wide'>Drag photo and videos here</h1>
+    <Uploader /> </>}
+    
+    
 
-    {/* <input 
-    type='file'
-    placeholder=''
-    className='text-sm bg-sky-600 px-4 py-1 rounded-md font-bold text-white' /> */}
 
     </div></div>
   
