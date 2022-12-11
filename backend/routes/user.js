251d9d9769c396b3b9d28b9636d935309f236837
Michael Kaminski <michael_kaminski@outlook.com>
2022-12-11 16:40:25 +0900

    backend returns if profile is yours or not, ui too

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import { registerNewUser, loginUser, getUserbyUsername,  } from '../controllers/user.js'
+import { requireLogin } from '../utils/auth.js'
 
 
 const router = express.Router()
@@ -8,8 +9,9 @@ router.use(cors())
 
 router.post('/register', registerNewUser)
 router.post('/login', loginUser)
+router.use(requireLogin)
 router.get('/:username', getUserbyUsername)
-router.post('/follow/:username')
-router.post('/unfollow/:username')
+router.post('/:username/follow')
+router.post('/:username/unfollow')
 
 export default router
