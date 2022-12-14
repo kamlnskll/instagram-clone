90f39623aec18d3c52639907cf43d64474eefbbb
Michael Kaminski <michael_kaminski@outlook.com>
2022-12-15 00:05:33 +0900

    post upload styling changed V. UGLY

--- frontend/src/components/Uploader.tsx
+++ frontend/src/components/Uploader.tsx
@@ -24,41 +24,6 @@ const [image, setImage] = useState('')
 // @ts-ignore 
 const [imageName, setImageName] = useState('')
 
-// const handleFileInputChange = (e: any) => {
-//     const file = e.target.files[0]
-//     previewFile(file)
-//     setSelectedFile(file)
-//     setFileInput(e.target.value)
-// }
-
-// const previewFile = (file: any) => {
-// const reader = new FileReader()
-// reader.readAsDataURL(file)
-// reader.onloadend = () => {
-// // @ts-ignore
-// setPreviewSource(reader.result)
-// }
-// }
-
-// const handleFileSubmit = (e: any) => {
-//     e.preventDefault()
-//     if(!selectedFile) return;
-//     const reader = new FileReader()
-//         // @ts-ignore
-//     reader.readAsDataURL(selectedFile)
-//     reader.onloadend = () => {
-//         uploadImage(reader.result)
-//     };
-//     reader.onerror = () => {
-//         console.log('Something went wrong when uploading file')
-//     }}
-
-
-// const uploadImage = async (base64Img: any) => {
-// console.log(base64Img)
-// await imgData(base64Img).then(setAddCaption(true)).then(console.log('img data variable loaded I think')).catch((err: any) => console.log(err))
-
-// }
 
 const handleImg = (e: any) => {
     console.log(e.target.files)
@@ -79,33 +44,13 @@ const getLinkFromUpload = sendImg.data.data.image
 }
 
 
-
-// sendImgtoCloudinary(formData).then((res) => console.log(res))
-
-
-// Maybe for multistep form including caption it can be possible to save the base64 conversion to a context provider and then grab it from the other part of the form with the caption.
-// Not sure
-
   return (
-    // <div className=''>
-    //     <form onSubmit={handleFileSubmit}>
-    //     <input 
-    //     type='file'
-    //     className='text-sm bg-sky-600 px-4 py-1 rounded-md font-bold text-white' 
-    //     onChange={handleFileInputChange}
-    //     value={fileInput}
-    //     name='file'
-    //     />
-    //     <button className='flex border-2 mx-auto mt-4 px-2 rounded-lg font-semibold' type='submit' onClick={() => {console.log('Btn submitted')}}>Next</button>
-    //     </form>
+   
+        <form className='px-12 pt-8 pb-12' encType='multipart/form-data'>
+        
+        <input type='file' name='image' onChange={handleImg} className='py-4'/>
+        <button className='bg-sky-600 text-center text-sm py-1 w-full rounded-sm text-white font-bold' onClick={sendImgtoBackend} type='submit'>Submit your image</button>
 
-
-        <form encType='multipart/form-data'>
-        <div className='bg-blue-400 border p-3'>
-        <h1>TESTING PURPOSES ONLY FOR AXIOS</h1>
-        <input type='file' name='image' onChange={handleImg} />
-        <button onClick={sendImgtoBackend} type='submit'>SUBMIT TEST IMAGE!!!!</button>
-        </div>
         </form>
    
   )
