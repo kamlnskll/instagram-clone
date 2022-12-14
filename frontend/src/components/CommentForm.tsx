58a102c0685d0515b770c35a45a6c42293e2b3e6
Michael Kaminski <michael_kaminski@outlook.com>
2022-12-15 00:28:21 +0900

    basic styling

--- frontend/src/components/CommentForm.tsx
+++ frontend/src/components/CommentForm.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
-const CommentForm = () => {
+type Props = {
+isOpen: boolean
+
+}
+const CommentForm = ({isOpen}: Props) => {
   return (
-    <div className='hidden' >Add a comment...</div>
+    <div className={ isOpen ? `pr-4 pt-4 flex` : `hidden`}>
+<textarea className='border border-none bg-gray-50 w-full placeholder:text-sm outline-none text-sm'
+placeholder='Add a comment...'/> 
+</div>
   )
 }
 
