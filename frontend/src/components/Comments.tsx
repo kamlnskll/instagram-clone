7229b2eab16f22e3a89e0a374ce1aa940a29293d
Michael Kaminski <michael_kaminski@outlook.com>
2022-12-20 01:16:02 +0900

    feat: comments are mapped to posts on f.e. + b.e.

--- frontend/src/components/Comments.tsx
+++ frontend/src/components/Comments.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router'
 import { getPostComments } from '../utils/axios/commentAPIs'
 
 
@@ -10,25 +11,35 @@ postId: any
 const Comments = ({postId}: Props) => {
 
 const [comments, setComments] = useState([])
+const [loading, setLoading] = useState(true)
+const navigate = useNavigate()
+
 
 useEffect(() => {
-getPostComments(postId).then((res) => {setComments(res)
-console.log(res)})
+getPostComments(postId).then((res) => { 
+setComments(res[0].comments)
+setLoading(false)}
+)
 
 }, [])
 
-  return (
-    <div>
-{/* {comments.map((comment: any) => {
-return (
-<div key={comment._id}>
-<img src={comment.postedBy.profilePic}/>
-<h1>{comment.postedBy.userName}</h1>
-<h1>{comment.comment}</h1>
-</div>
-)})} */}
 
-<button type='button' onClick={() => console.log(comments)}>Test</button>
+if(loading){
+  return <h1>Loading comments...</h1>
+}
+
+  return (
+    <div className='pt-2'>
+{comments.map((comment: any) => {
+return (
+<div key={comment._id} className='flex text-sm gap-2'>
+{/* <img className='w-[12px] h-[12px] rounded-full'src={comment.postedBy.profilePic}/> */}
+<h1 onClick={() => navigate(`profile/${comment.postedBy.userName}`)} className='font-semibold cursor-pointer'>{comment.postedBy.userName}</h1>
+<h1 className=''>{comment.comment}</h1>
+</div>
+)})}
+
+{/* <button type='button' onClick={() => console.log(comments)}>Test</button> */}
 
     </div>
 )
