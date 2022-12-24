1c3d903ef0d74362edff639339edee308dcddc39
Michael Kaminski <michael_kaminski@outlook.com>
2022-12-25 02:54:36 +0900

    added input field, adjusted height

--- frontend/src/components/ChatContainer.tsx
+++ frontend/src/components/ChatContainer.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import io from 'socket.io-client'
 
 const ChatContainer = () => {
+
+const socket = io('http://localhost:8001')
+
+
+
+
   return (
-    <h1>Chatcontainer</h1>
+    <div className='border h-full'>
+    <h1 className=''>Chat Container
+      <input placeholder='Message...'className='border rounded-full py-2 pl-6 outline-none text-sm w-5/6'/>
+    </h1>
+    </div>
   )
 }
 
--- frontend/src/pages/Messages.tsx
+++ frontend/src/pages/Messages.tsx
@@ -1,19 +1,17 @@
 import React from 'react'
-import io from 'socket.io-client'
 import ChatContainer from '../components/ChatContainer'
 import Navbar from '../components/Navbar'
 
 
 const Messages = () => {
 
-const socket = io('http://localhost:8001')
 
   return (
     <div className='flex'>
       <Navbar />
       <div className='w-full'>
       <div className='bg-white border w-3/4 mx-auto h-5/6 my-16 rounded-md'>
-      <div className='grid grid-cols-3'>
+      <div className='grid grid-cols-3 h-full'>
       <div className='col-span-1 bg-blue-200'>
         <h1>List of people you are messaging...</h1>
       </div>
