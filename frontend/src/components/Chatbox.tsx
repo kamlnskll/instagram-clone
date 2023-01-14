c938ea07e4a862091a71c6320459d75967e6766c
Michael Kaminski <michael_kaminski@outlook.com>
2023-01-14 22:38:01 +0900

    added navigation to other user in chat on click

--- frontend/src/components/Chatbox.tsx
+++ frontend/src/components/Chatbox.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router'
 
 type Props = {
 chat: any,
@@ -10,18 +11,20 @@ key: any,
 const Chatbox = ({chat, userId, chatLength, key}: Props) => {
 
 const [isOwnUser, setIsOwnUser] = useState(false)
+const navigate = useNavigate()
+
 
 // Right now, the chat.sender ID is received first and compares to the UserID but the chat.sender._id is not received so everything else is not filled
 // So the chat is on the correct side but I need to wait for the sender field to be populated.
 
 useEffect(() => {
-  if(chat && chat.sender._id === userId || chat.sender == userId){
+  if(chat && chat.sender._id === userId){
     setIsOwnUser(true)
     }
     else {
     setIsOwnUser(false)
     }
-console.log(chat)
+// console.log(chat)
 }, [chatLength])
 
   return (
@@ -31,9 +34,9 @@ console.log(chat)
     <h1 className='text-center py-3 text-sm'>{chat.message}</h1>
   </div> ) : (
     <div className='ml-auto' key={key}>
-    <h1 className='text-gray-400 text-xs pl-2 pb-1'>{chat.sender.userName}</h1>
+    <h1 className='text-gray-400 text-xs pl-2 pb-1 cursor-pointer' onClick={() => navigate(`/profile/${chat.sender.userName}`)}>{chat.sender.userName}</h1>
     <div className='flex gap-1'>
-    <img src={chat.sender.profilePic} className='mt-auto rounded-full w-[28px] h-[28px]' />
+    <img src={chat.sender.profilePic} className='mt-auto rounded-full w-[28px] h-[28px] cursor-pointer' onClick={() => navigate(`/profile/${chat.sender.userName}`)}/>
     <div className='bg-white border rounded-full w-fit px-4' key={chat}>
     <h1 className='py-3 text-sm'>{chat.message}</h1>
     </div>
