7dd8a22bf43db0a555cb50322d4fd2fd75ad247e
Michael Kaminski <michael_kaminski@outlook.com>
2023-01-15 01:26:29 +0900

    fixed conditional rendering of each component

--- frontend/src/components/NewConvoModal.tsx
+++ frontend/src/components/NewConvoModal.tsx
@@ -20,15 +20,11 @@ const handleSearch = (searchResults: any) => {
         setLoading(false)
         }
 
-useEffect(() => {
-console.log(search)
+const handleSubmit = () => {
 
-}, [handleSearch])
 
-useEffect(() => {
-    console.log(user)
-    
-    }, [user])
+}
+
   return (
     isOpen ? (
         <div className='w-1/2 h-1/2 bg-white border rounded-md absolute z-20 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
@@ -36,8 +32,12 @@ useEffect(() => {
         <div className='px-6 pt-4'>
         <SearchInput onSearch={handleSearch}/>
         </div>
-        <div>
+        <div className=''>
+        
+        {Object.keys(user).length == 0 ? (
+            <>
         <h1 className='font-semibold text-center'>Results</h1>
+        <div>
         {search.filter((user: any) => user._id != userId).map((user: any) => (
 
             <>
@@ -52,24 +52,19 @@ useEffect(() => {
 
         ))}
         </div>
-        <div>
-            {/* {user && Object.keys(user).length !== 0 && user.hasOwnProperty('userName') &&  user.hasOwnProperty('profilePic') ? (
-             <>
-            <div className='hover:bg-gray-100 py-2 mx-2 my-1 rounded-xl cursor-pointer' onClick={() => setUser(user)}>
-            <div className='flex pl-4 gap-5'>
-            <img className='w-[24px] h-[24px] my-auto' src={user.profilePic}/>
-            <h1 className='text-lg pl-1'>{user.userName}</h1>
-            </div>
-            </div>
-            </>
-            ) : (
-                null
-            )} */}
+        </>
+        ) : (
+
+            <div>
             <h1 className='font-semibold text-center'>Type your first message</h1>
     <div className='flex relative'>
       <input placeholder='Message...' className='border rounded-full pl-6 h-[40px] mx-auto outline-none text-md w-5/6 my-auto' value={message} onChange={(e) => setMessage(e.target.value)}/>
       <button type='button' className={message !== '' ? `hover:bg-blue-500 bg-blue-400 px-2 py-1 absolute right-14 top-2 text-xs rounded-xl text-white font-semibold` : `hidden`}>Send</button>
       </div>
+        </div>
+
+        )}
+        
         </div>
         </div>
 
